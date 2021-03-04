897b0d42150fd5fc0c280930ee45480337a9f24f
marco-dm1 <73443992+marco-dm1@users.noreply.github.com>
2021-03-04 17:35:53 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,18 @@
+var compatibleUnits = {
+    mile: 1,
+    kilometer: 1,
 
+}
+var outputValue = document.getElementById("convertedValue");
+
+function convertValues(){
+    let initialValue = document.getElementById("initialValue").value;
+    let initialUnit = document.getElementById("initialUnit").value;
+    let convertUnit = document.getElementById("convertUnit").value;
+    if(compatibleUnits[convertUnit] == compatibleUnits[initialUnit]){
+        
+    } else{
+        outputValue.value = null;
+        outputValue.placeholder = "Units invalid!";
+    }
+}
