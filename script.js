f8d074c2e74cdd85327767af1ea58bde2685d9b9
marco-dm1 <73443992+marco-dm1@users.noreply.github.com>
2021-03-17 09:42:27 -0400

    Final commit
    Added conversion for the two units and removed old and unnecessary code.

--- script.js
+++ script.js
@@ -1,18 +1,20 @@
-var compatibleUnits = {
-    mile: 1,
-    kilometer: 1,
-
-}
 var outputValue = document.getElementById("convertedValue");
 
 function convertValues(){
     let initialValue = document.getElementById("initialValue").value;
     let initialUnit = document.getElementById("initialUnit").value;
     let convertUnit = document.getElementById("convertUnit").value;
-    if(compatibleUnits[convertUnit] == compatibleUnits[initialUnit]){
-        
-    } else{
-        outputValue.value = null;
-        outputValue.placeholder = "Units invalid!";
+    if(initialValue){
+        if(initialUnit == convertUnit){
+            outputValue.value = initialValue
+        }
+        else if(initialUnit == "mile"){
+            if(convertUnit = "kilometer"){
+                outputValue.value = (initialValue * 1.60934)
+            }
+        }
+        else{
+            outputValue.value = (initialValue * 0.621371)
+        }
     }
 }
